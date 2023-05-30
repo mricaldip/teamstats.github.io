1ee8c01a6e92a15040663957ed92227d234396e4
Mauricio Ricaldi <76887915+mricaldip@users.noreply.github.com>
2023-05-29 20:30:00 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,27 @@
+const menu = {
+    _meal: '',
+    _price: 0,
+
+    set meal(mealToCheck) {
+     if (typeof mealToCheck === 'string') {
+      return this._meal = mealToCheck
+      }
+    },
+
+    set price(priceToCheck) {
+      if (typeof priceToCheck === 'number') {
+      return this._price = priceToCheck;
+      }
+    },
+
+    get todaysSpecial(){
+      if (this._meal && this._price) {
+        return `Today’s Special is ${this._meal} for $${this._price}!`
+      } else {
+        return `Meal or price was not set correctly!`
+      }
+    }
+};
+menu.meal = 'arroz con pollo';
+menu.price = 25;
+console.log(menu.todaysSpecial);
